e20af818630a8c20e46207bf696d745341f9ca0d
Shruthi Bandyadka <shruthibhat93@gmail.com>
2024-05-02 11:17:21 -0400

    batch2 samples

--- code/quantify_hemocyteFITC_intensity.groovy
+++ code/quantify_hemocyteFITC_intensity.groovy
@@ -3,11 +3,11 @@
 // with ROI and object detections overlayed.
 
 // Author : Shruthi Bandyadka (sbandya@bu.ed), McCall Lab, Boston University 
+import qupath.lib.gui.scripting.QPEx
 
-
-annotation_measurements = "/Users/sbandya/Desktop/dev/hemocyte_ImageSegmentation/analysis/annotation_measurements"
-detection_measurements = "/Users/sbandya/Desktop/dev/hemocyte_ImageSegmentation/analysis/detection_measurements"
-annotated_images = "/Users/sbandya/Desktop/dev/hemocyte_ImageSegmentation/analysis/annotated_images"
+annotation_measurements = "/Users/sbandya/Desktop/hemocyte_recruitment_image_quantification/data/batch2/annotation_measurements"
+detection_measurements = "/Users/sbandya/Desktop/hemocyte_recruitment_image_quantification/data/batch2/detection_measurements"
+annotated_images = "/Users/sbandya/Desktop/hemocyte_recruitment_image_quantification/data/batch2/annotated_images"
 
 def imageData = QPEx.getCurrentImageData()
 def server = imageData.getServer()
@@ -19,7 +19,7 @@ String filename = path_without_extension[path_without_extension.lastIndexOf('/')
 
 setImageType('FLUORESCENCE');
 runPlugin('qupath.lib.plugins.objects.FillAnnotationHolesPlugin', '{}');
-runPlugin('qupath.imagej.detect.cells.PositiveCellDetection', '{"detectionImage": "Green",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 7.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 0.5,  "minAreaMicrons": 1.0,  "maxAreaMicrons": 400.0,  "threshold": 25.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Nucleus: Green max",  "thresholdPositive1": 10.0,  "thresholdPositive2": 20.0,  "thresholdPositive3": 30.0,  "singleThreshold": true}');
+runPlugin('qupath.imagej.detect.cells.PositiveCellDetection', '{"detectionImage": "Green",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 7.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 0.5,  "minAreaMicrons": 1.0,  "maxAreaMicrons": 400.0,  "threshold": 50.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Nucleus: Green max",  "thresholdPositive1": 10.0,  "thresholdPositive2": 20.0,  "thresholdPositive3": 30.0,  "singleThreshold": true}');
 runPlugin('qupath.opencv.features.DelaunayClusteringPlugin', '{"distanceThresholdMicrons": 0.0,  "limitByClass": false,  "addClusterMeasurements": false}');
 saveAnnotationMeasurements("${annotation_measurements}/${filename}.txt")
 saveDetectionMeasurements("${detection_measurements}/${filename}.txt")
